f3faa5a94fe1aa75ce4c2753f29ac43fbf16cad0
Ceora Ford <ceoraallthetime@gmail.com>
2018-07-24 13:16:07 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -37,7 +37,7 @@ puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize # Put a . after the string to capitalize it properly.
 
 puts proper_sentence
 
@@ -46,7 +46,7 @@ sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples 
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+sentence = sentence.gsub # Add your gsub chain here
 
 puts sentence
 
